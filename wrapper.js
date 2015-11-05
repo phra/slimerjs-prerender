1d8b4ee5ff733f4ad58f493700d55de1c0f38f10
phra <phra>
2015-11-05 23:53:34 +0000

    start xvfb-run w/ -a

--- wrapper.js
+++ wrapper.js
@@ -2,6 +2,6 @@ var path = require('path');
 var spawn = require('child_process').spawn;
 
 var binPath = path.resolve(__dirname, 'index.js');
-var args = ['node', 'index.js'];
+var args = ['-a', 'node', 'index.js'];
 
 var cp = spawn('xvfb-run', args, {stdio: 'inherit'});
